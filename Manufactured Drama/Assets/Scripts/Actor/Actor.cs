32181385929c9d511dbdb89a7e00eccd532535bb
Kent-Desktop\Kent <theswordbattler@gmail.com>
2017-11-20 00:21:42 -0600

    The audio mixer now has groups for each character, for music, and for sfx. I started writing the actor system that will handle events from the animations. I also created the scriptableobject for dialogue data lists for easy construction for each character.

---
+++ Manufactured Drama/Assets/Scripts/Actor/Actor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour {
+
+	/* The actor's dialogue data for the entire scene */
+	public DialogueDataList dialogueData;
+
+	/* The actor's audio source */
+	private AudioSource audioSrc;
+
+	// Use this for initialization
+	void Start () {
+		audioSrc = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void PlayNextLine() {
+		dialogueData.PlayLine (audioSrc);
+	}
+}
---
+++ Manufactured Drama/Assets/Scripts/Actor/DialogueDataList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DialogueDataList : ScriptableObject {
+
+	public int lineIndex;
+
+	public List<DialogueLine> lines;
+
+	public void PlayLine(AudioSource audioSrc) {
+		audioSrc.clip = lines [lineIndex].audio;
+		audioSrc.Play ();
+	}
+
+	public string GetLineText() {
+		return lines [lineIndex].text;
+	}
+}
